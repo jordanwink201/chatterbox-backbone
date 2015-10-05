91eb0d628780fc1f9b516ac9054109ce52a220e9
Jordan Winkelman <jordanwink201@gmail.com>
2015-10-04 17:13:03 -0700

    styled message app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,6 @@
 // Fixes the padding for the .current selector of the 'li' item
 // $('.current i').css('padding-left','5.2em');
+
+
+var position = $('.tab-content').position();
+$('.list-group').first().css('margin-top', position.top);
