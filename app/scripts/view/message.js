8fe79939953d49ae7e92ff5a87dcdd2ea02e9bd1
Jordan Winkelman <jordanwink201@gmail.com>
2015-10-06 22:06:48 -0700

    changed to use a template script for the messages

--- app/scripts/view/message.js
+++ app/scripts/view/message.js
@@ -2,13 +2,7 @@ var MessageView = Backbone.View.extend({
 
   tagName : 'tr',
 
-  template : _.template('<td><%= createdAt %></td> \
-    <td><%= (room ? room : "") %></td> \
-    <td><%= (username ? username : "") %></td> \
-    <td><%= (text ? text : "") %></td> \
-    <td><a class="noDec" href=""><i class="icon-heart"></i></a></td> \
-    <td><a class="noDec" href=""><i class="icon-budicon-32"></i></a></td> \
-  '),
+  template : _.template( $('#message').html(), {} ),
 
   events : {
     'click .icon-heart' : 'toggleFriend',
