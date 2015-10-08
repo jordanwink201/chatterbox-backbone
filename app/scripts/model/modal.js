8831e1ba6c436feea2ea3f9298d815bedb1937e9
Jordan Winkelman <jordanwink201@gmail.com>
2015-10-07 23:01:26 -0700

    added modal, need to do events based on modal events

--- app/scripts/model/modal.js
+++ app/scripts/model/modal.js
@@ -1,7 +1,7 @@
 var Modal = Backbone.Model.extend({
 
   defaults : {
-    title : 'default',
+    title : 'Pick a Room to Enter',
     roomList : ['room1', 'room2', 'room3']
   }
 
--- app/scripts/view/modalView.js
+++ app/scripts/view/modalView.js
@@ -2,6 +2,10 @@ var ModalView = Backbone.View.extend({
 
   template : _.template( $('#modalTemplate').html() ),
 
+  events : {
+    'click a' : 'getRoom'
+  },
+
   initialize : function(){
     this.render();
   },
@@ -9,6 +13,13 @@ var ModalView = Backbone.View.extend({
   render : function(){
     console.log('this model attributes : ', this.model.attributes);
     return this.$el.html( this.template(this.model.attributes) );
+  },
+
+  getRoom : function(){
+    // close the modal
+    console.log("this : ", this.model);
+    // get the specified roomname
+    console.log('get the room');
   }
 
 });
